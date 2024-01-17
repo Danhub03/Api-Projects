37ff0ee5ea4e479e8b35d9398428be98c7d455ae
Danhub03 <118169550+Danhub03@users.noreply.github.com>
2024-01-17 11:36:13 +0100

    Update calendar.js

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -1,9 +1,3 @@
-// fetch(("https://www.googleapis.com/calendar/v3/calendars/c_1f9277f62b81da3cc239dbf21c2c483b367df96d0cd06f8933f0c203ba3418f6@group.calendar.google.com/events?key=")
-
-// fetch( "https://www.googleapis.com/calendar/v3/calendars/c_d9aaaa6aa5b776b23b57ec82ab49a0b39b34177b8390aa055f926d10033e3648@group.calendar.google.com/events?key=AIzaSyB6mHlr2Yt1DsSaHU3RCHWKyyZqvOf4eok")
-// .then( res => res.json())
-// .then(res => console.log(res))
-
 fetch("https://www.googleapis.com/calendar/v3/calendars/c_d9aaaa6aa5b776b23b57ec82ab49a0b39b34177b8390aa055f926d10033e3648@group.calendar.google.com/events?key=AIzaSyB6mHlr2Yt1DsSaHU3RCHWKyyZqvOf4eok")
   .then(res => res.json())
   .then(data => {
